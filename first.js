33c4dae291ae56e8c6815251df155f7c58b5092b
rishitha2212 <45210901+rishitha2212@users.noreply.github.com>
2021-05-26 11:25:49 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+/*console.log("ex js file");
+function my_fn(){
+	console.log("fnt called");
+	document.getElementById("1").innerHTML="changes by fnt tag";
+}
+function fn2(){
+	var a,b,c;
+	a=5;
+	b="hello"
+	document.getElementById("1").innerHTML=b;
+}*/
+
+var a=1;
+function fn3(){
+ a++;
+ document.getElementById("sample_image").src="pic"+a+".jpg";
+ 
+}
